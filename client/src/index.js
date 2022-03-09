d779a27c2804cdad59dc7a18231dd93455b9ac06
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-09 14:59:11 -0800

    Add suspense with loader for routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import Context from './Context/index';
+import Routes from 'Routes';
+import Page from 'components/Page';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'react-image-lightbox/style.css';
+import 'aos/dist/aos.css';
+import { CircularProgress } from '@mui/material';
+
 ReactDOM.render(
   <BrowserRouter>
     <Context>
-      <App />
+      <Suspense
+        fallback={
+          <CircularProgress
+            style={{
+              margin: 'auto',
+              left: '0',
+              right: '0',
+              top: '0',
+              bottom: '0',
+              position: 'fixed',
+            }}
+          />
+        }
+      >
+        <Page>
+          <Routes />
+        </Page>
+      </Suspense>
     </Context>
   </BrowserRouter>,
   document.getElementById('root'),
