32230568384b18a2c321431d22e3e8b62deedd8e
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-10 19:35:10 -0800

    Update Signin.js

--- client/src/views/Signin/Signin.js
+++ client/src/views/Signin/Signin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -15,50 +15,52 @@ const Signin = () => {
 
   return (
     <Main>
-      <Box
-        position={'relative'}
-        minHeight={'calc(100vh - 247px)'}
-        display={'flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        height={1}
-      >
-        <Container>
-          <Grid container spacing={6}>
-            {isMd ? (
-              <Grid item container justifyContent={'center'} xs={12} md={6}>
-                <Box height={1} width={1} maxWidth={500}>
-                  <Box
-                    component={'img'}
-                    src={
-                      'https://assets.maccarianagency.com/svg/illustrations/drawkit-illustration2.svg'
-                    }
-                    width={1}
-                    height={1}
-                    sx={{
-                      filter:
-                        theme.palette.mode === 'dark'
-                          ? 'brightness(0.8)'
-                          : 'none',
-                    }}
-                  />
-                </Box>
-              </Grid>
-            ) : null}
+      <Suspense fallback={<div>Loading</div>}>
+        <Box
+          position={'relative'}
+          minHeight={'calc(100vh - 247px)'}
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          height={1}
+        >
+          <Container>
+            <Grid container spacing={6}>
+              {isMd ? (
+                <Grid item container justifyContent={'center'} xs={12} md={6}>
+                  <Box height={1} width={1} maxWidth={500}>
+                    <Box
+                      component={'img'}
+                      src={
+                        'https://assets.maccarianagency.com/svg/illustrations/drawkit-illustration2.svg'
+                      }
+                      width={1}
+                      height={1}
+                      sx={{
+                        filter:
+                          theme.palette.mode === 'dark'
+                            ? 'brightness(0.8)'
+                            : 'none',
+                      }}
+                    />
+                  </Box>
+                </Grid>
+              ) : null}
 
-            <Grid
-              item
-              container
-              alignItems={'center'}
-              justifyContent={'center'}
-              xs={12}
-              md={6}
-            >
-              <Form />
+              <Grid
+                item
+                container
+                alignItems={'center'}
+                justifyContent={'center'}
+                xs={12}
+                md={6}
+              >
+                <Form />
+              </Grid>
             </Grid>
-          </Grid>
-        </Container>
-      </Box>
+          </Container>
+        </Box>
+      </Suspense>
     </Main>
   );
 };
