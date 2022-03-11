f38a340d969bf703197bc8dd71acac67192555b6
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-10 19:33:07 -0800

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,61 +1,131 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import UserModal from '../models/userModel.js';
-dotenv.config();
-const secret = process.env.JWT_SECRET;
-console.log('secret', secret);
-export const signin = async (req, res) => {
-	const { email, password } = req.body;
+import User from '../models/userModel.js';
+import ShortLink from '../models/shortLinkModel.js';
 
+// export const login = async (req, res, next) => {
+// 	passport.authenticate('local');
+
+// 	const token = getToken({ _id: req.user._id });
+// 	const refreshToken = getRefreshToken({ _id: req.user._id });
+// 	User.findById(req.user._id).then(
+// 		(user) => {
+// 			user.refreshToken.push({ refreshToken });
+// 			user.save((err, user) => {
+// 				if (err) {
+// 					res.status(500).json(err);
+// 				} else {
+// 					res.cookie('refreshToken', refreshToken, COOKIE_OPTIONS);
+// 					res.send({ success: true, token });
+// 				}
+// 			});
+// 		},
+// 		(err) => next(err)
+// 	);
+// };
+// export const signup = async (req, res) => {
+// 	const { firstName, lastName, email, password } = req.body;
+// 	try {
+// 		User.register(new User({ email: email }), password, (error, user) => {
+// 			if (error) {
+// 				res.status(500).json(error);
+// 				console.error(error);
+// 			} else {
+// 				user.firstName = firstName || '';
+// 				user.lastName = lastName || '';
+// 				user.name = `${firstName} ${lastName}`;
+
+// 				user.save((error, user) => {
+// 					if (error) {
+// 						res.status(500).json(error);
+// 						console.error(error);
+// 					} else {
+// 						res.cookie('refreshToken', refreshToken, COOKIE_OPTIONS);
+// 						res.send({ success: true, token });
+// 					}
+// 				});
+// 			}
+// 		});
+// 	} catch (error) {
+// 		res.status(500).json(error);
+// 	}
+// };
+
+// export const generateRefreshToken = async (req, res, next) => {
+// 	const { signedCookies = {} } = req;
+// 	const { refreshToken } = signedCookies;
+
+// 	if (refreshToken) {
+// 		try {
+// 			const payload = jwt.verify(
+// 				refreshToken,
+// 				process.env.REFRESH_TOKEN_SECRET
+// 			);
+// 			const userId = payload._id;
+// 			User.findOne({ _id: userId }).then(
+// 				(user) => {
+// 					if (user) {
+// 						// Find the refresh token against the user record in database
+// 						const tokenIndex = user.refreshToken.findIndex(
+// 							(item) => item.refreshToken === refreshToken
+// 						);
+
+// 						if (tokenIndex === -1) {
+// 							res.statusCode = 401;
+// 							res.send('Unauthorized');
+// 						} else {
+// 							const token = getToken({ _id: userId });
+// 							// If the refresh token exists, then create new one and replace it.
+// 							const newRefreshToken = getRefreshToken({ _id: userId });
+// 							user.refreshToken[tokenIndex] = { refreshToken: newRefreshToken };
+// 							user.save((err, user) => {
+// 								if (err) {
+// 									res.status(500).json(err);
+// 								} else {
+// 									res.cookie('refreshToken', newRefreshToken, COOKIE_OPTIONS);
+// 									res.send({ success: true, token });
+// 								}
+// 							});
+// 						}
+// 					} else {
+// 						res.status(401).json('Unauthorized');
+// 					}
+// 				},
+// 				(err) => next(err)
+// 			);
+// 		} catch (err) {
+// 			res.statusCode = 401;
+// 			res.send('Unauthorized');
+// 		}
+// 	} else {
+// 		res.statusCode = 401;
+// 		res.send('Unauthorized');
+// 	}
+// };
+export const getUserStats = async (req, res) => {
 	try {
-		const oldUser = await UserModal.findOne({ email });
-
-		if (!oldUser)
-			return res.status(404).json({ message: "User doesn't exist" });
-
-		const isPasswordCorrect = await oldUser.matchPassword(password);
-		console.log(isPasswordCorrect);
-		if (!isPasswordCorrect)
-			return res.status(400).json({ message: 'Invalid credentials' });
-
-		const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
-			expiresIn: '1d',
+		await User.findById(req.user._id);
+		const Analytics = await ShortLink.find({ user: req.user._id }).populate({
+			path: 'analytics',
+			options: { sort: { created_at: -1 } },
 		});
-		oldUser.tokens = token;
-		res.status(200).json({ result: oldUser });
-	} catch (err) {
-		res.status(500).json({ message: 'Something went wrong' });
-	}
-};
-
-export const signup = async (req, res) => {
-	const { email, password, firstName, lastName } = req.body;
-
-	try {
-		const oldUser = await UserModal.findOne({ email });
-
-		if (oldUser)
-			return res.status(400).json({ message: 'User already exists' });
-
-		const hashedPassword = await bcrypt.hash(password, 12);
-
-		const result = await UserModal.create({
-			email,
-			password: hashedPassword,
-			name: `${firstName} ${lastName}`,
-			firstName: firstName,
-			lastName: lastName,
+		let totalClicks = 0;
+		await Analytics.forEach(async (ShortLink) => {
+			return (totalClicks = (await ShortLink.analytics.clicks) + totalClicks);
 		});
-		const token = jwt.sign({ email: result.email, id: result._id }, secret, {
-			expiresIn: '1h',
+		const totalLinks = Analytics.length;
+		let AllLocations = [];
+		await Analytics.map((Link) => {
+			if (Link.analytics.location.length !== 0)
+				AllLocations.push(Link.analytics.location);
 		});
-		result.tokens = token;
-		result.save();
 
-		res.status(201).json({ result });
+		return res.status(200).json({
+			Analytics: Analytics,
+			TotalClicks: totalClicks,
+			TotalLinks: totalLinks,
+			AllLocations: AllLocations,
+		});
 	} catch (error) {
-		res.status(500).json({ message: 'Something went wrong' });
+		res.status(500).json({ message: 'Something went wrong', data: { error } });
 
 		console.log(error);
 	}
