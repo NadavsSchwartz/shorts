5b07304b330182a0fe3a3db2a2c2bc2504f87b53
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-11 17:49:15 -0800

    update ShortLinkModel to include the original site's icon

--- server/models/shortLinkModel.js
+++ server/models/shortLinkModel.js
@@ -16,6 +16,10 @@ const linkSchema = mongoose.Schema(
 			required: true,
 			unique: true,
 		},
+		siteIcon: {
+			type: String,
+			required: true,
+		},
 		user: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'User',
