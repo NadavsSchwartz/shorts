6756db516a03b758706486d5b563da6a34f6804c
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-19 18:40:24 -0700

    prettier codestyle fix for server

--- server/models/analyticsModel.js
+++ server/models/analyticsModel.js
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const analyticsSchema = mongoose.Schema(
-	{
-		totalClicks: {
-			type: Number,
-			required: true,
-			default: 0,
-		},
-		shortLink: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'ShortLink',
-		},
-		location: {
-			type: Array,
-			require: false,
-		},
-		clicks: { type: Array, require: true },
-	},
-	{ timestamps: true }
+  {
+    totalClicks: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shortLink: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ShortLink",
+    },
+    location: {
+      type: Array,
+      require: false,
+    },
+    clicks: { type: Array, require: true },
+  },
+  { timestamps: true }
 );
-const Analytics = mongoose.model('Analytics', analyticsSchema);
+const Analytics = mongoose.model("Analytics", analyticsSchema);
 export default Analytics;
