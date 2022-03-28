a11bee6d73d88bfe731edb97fd9591cfe57dfa68
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-27 18:47:20 -0700

    prettier styling changes

--- models/userModel.js
+++ models/userModel.js
@@ -1,53 +1,57 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      default: '',
-    },
-    firstName: {
-      type: String,
-      default: '',
-    },
-    lastName: {
-      type: String,
-      default: '',
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    authority: {
-      type: String,
-    },
+    {
+        name: {
+            type: String,
+            default: '',
+        },
+        firstName: {
+            type: String,
+            default: '',
+        },
+        lastName: {
+            type: String,
+            default: '',
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        authority: {
+            type: String,
+        },
 
-    googleId: {
-      type: String,
-      default: '',
-    },
+        googleId: {
+            type: String,
+            default: '',
+        },
 
-    avatar: {
-      type: String,
+        avatar: {
+            type: String,
+        },
+        twitterId: {
+            required: false,
+            type: String,
+        },
+        githubId: {
+            required: false,
+            type: String,
+        },
+        apiKey: {
+            required: false,
+            type: String,
+        },
     },
-    twitterId: {
-      required: false,
-      type: String,
-    },
-    githubId: {
-      required: false,
-      type: String,
-    },
-  },
-  { timestamps: true, toJSON: { virtuals: true } },
-);
+    { timestamps: true, toJSON: { virtuals: true } }
+)
 
 userSchema.virtual('ShortLinks', {
-  ref: 'ShortLink',
-  foreignField: 'user',
-  localField: '_id',
-});
-const User = mongoose.model('User', userSchema);
+    ref: 'ShortLink',
+    foreignField: 'user',
+    localField: '_id',
+})
+const User = mongoose.model('User', userSchema)
 
-export default User;
+export default User
