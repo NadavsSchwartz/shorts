7450219ae50f7c41d34673a5e0b6d31200e361b4
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-27 18:46:06 -0700

    Update errorMiddleware.js

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -6,8 +6,7 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
+    return res.status(statusCode).json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     })
