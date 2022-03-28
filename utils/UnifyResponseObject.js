fb46507238831c56ae981e7ac675481a26a67ea5
nadavschwartz <nadavschwartz58@gmail.com>
2022-03-27 18:45:59 -0700

    added a response object unifier utility function

---
+++ utils/UnifyResponseObject.js
@@ -0,0 +1,26 @@
+export const UnifyResponseObject = async (Analytics) => {
+    let AllClicks = 0
+    const AllLocations = []
+    const modifiedAnalytics = []
+    await Analytics.map((Link) => {
+        // calculate total clicks of all links for easy management in front end
+        AllClicks = Link.analytics.totalClicks + AllClicks
+
+        // calculate all locations of each non empty clicks location for easy management in front end
+        if (Link.analytics.location.length !== 0)
+            AllLocations.push(Link.analytics.location)
+
+        // modify the array of object of ShortLinks and Analytics for easier management in front end
+        modifiedAnalytics.push({
+            id: Link._id,
+            shortUrl: Link.shortUrl,
+            siteIcon: Link.siteIcon,
+            longUrl: Link.longUrl,
+            createdAt: Link.createdAt,
+            totalClicks: Link.analytics.totalClicks,
+            clicks: Link.analytics.clicks,
+            location: Link.analytics.location,
+        })
+    })
+    return modifiedAnalytics
+}
